d9bbedb9d70e9f87e9b232de14e2d13998fd3296
sahilburle <sahilburle1998@gmail.com>
2022-07-11 09:29:31 +0530

    stream api

---
+++ StreamAPI/src/work/StreamMain1.java
@@ -0,0 +1,18 @@
+package src.work;
+
+import java.util.List;
+
+public class StreamMain1 {
+
+    public static void main(String[] args) {
+
+        // Q) create list and filter all even numbers from list
+        //
+
+        List<Integer> list1 = List.of(2, 7, 58, 52, 63, 41, 65);
+
+        System.out.println(list1);
+
+
+    }
+}
