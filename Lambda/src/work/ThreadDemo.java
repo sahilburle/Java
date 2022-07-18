e3291778b502b998a4c7a7d011c5a42b00bf1662
sahilburle <sahilburle1998@gmail.com>
2022-07-18 12:32:37 +0530

    lambda

--- Lambda/src/work/ThreadDemo.java
+++ Lambda/src/work/ThreadDemo.java
@@ -8,8 +8,36 @@ public class ThreadDemo {
         Runnable thread1 = () -> {
             //this is body of thread
             //stuff
-            
+            for (int i =1; i <= 10; i++){
+                System.out.println(i);
+                try {
+                    Thread.sleep(1000
+                    );
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         };
+
+        Thread t = new Thread(thread1);
+        t.setName("JOHN");
+        t.start();
+
+
+        Runnable t2 = () -> {
+            try {
+                for (int i = 1; i <= 10; i++){
+                    System.out.println(i * 2);
+                    Thread.sleep(2000);
+                }
+            }catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        };
+
+        Thread T2= new Thread(t2);
+        t.start();
 
     }
 }
