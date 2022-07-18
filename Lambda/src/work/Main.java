3af7d62be8d178898735d06934c39f2f8437a91d
sahilburle <sahilburle1998@gmail.com>
2022-07-18 09:33:18 +0530

    lambda

---
+++ Lambda/src/work/Main.java
@@ -0,0 +1,8 @@
+package src.work;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("My system starts..");
+    }
+}
---
+++ Lambda/src/work/MyInter.java
@@ -0,0 +1,4 @@
+package src.work;
+
+public interface MyInter {
+}
